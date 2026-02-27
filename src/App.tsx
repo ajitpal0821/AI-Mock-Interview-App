a4095242fe40e8f4fec6673865ab9f20bb900710
ajitpal0821 <107208518+ajitpal0821@users.noreply.github.com>
2026-02-27 11:14:57 +0530

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='text-blue-500'>App
+    <Button>Click ME</Button>
+    </div>
+  )
+}
+
+export default App
